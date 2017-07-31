25aba5857b5086db6b59b15fc3f38f060058bb29
mirrete <mertloka@hotmail.com>
2017-07-31 03:20:32 +0300

    list_questions

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,4 +1,8 @@
+@extends('layouts.quiz_layout')
 
+@section('content')
+    @include('admin.admin_tab')
+    @include('settings.settings_tab')
 <h1>All the questions</h1>
 
 <!-- will be used to show any messages -->
@@ -10,29 +14,26 @@
 	<thead>
 		<tr>
 			<td>ID</td>
-			<td>Name</td>
-			<td>Email</td>
-			<td>question Level</td>
-			<td>Actions</td>
+			<td>question</td>
+			<td>question_type_id</td>
+			<td>category_id</td>
+			<td>points</td>
 		</tr>
 	</thead>
 	<tbody>
 	@foreach($questions as $key => $value)
 		<tr>
 			<td>{{ $value->id }}</td>
-			<td>{{ $value->name }}</td>
-			<td>{{ $value->email }}</td>
-			<td>{{ $value->question_level }}</td>
-
+			<td>{{ $value->question }}</td>
+			<td>{{ $value->question_type_id }}</td>
+			<td>{{ $value->category_id }}</td>
+            <td>{{ $value->points }}</td>
 			<!-- we will also add show, edit, and delete buttons -->
 			<td>
 
 				<!-- delete the question (uses the destroy method DESTROY /questions/{id} -->
 				<!-- we will add this later since its a little more complicated than the first two buttons -->
-				{{ Form::open(array('url' => 'questions/' . $value->id, 'class' => 'pull-right')) }}
-					{{ Form::hidden('_method', 'DELETE') }}
-					{{ Form::submit('Delete this question', array('class' => 'btn btn-warning')) }}
-				{{ Form::close() }}
+				
 
 				<!-- show the question (uses the show method found at GET /questions/{id} -->
 				<a class="btn btn-small btn-success" href="{{ URL::to('questions/' . $value->id) }}">Show this question</a>
@@ -45,4 +46,4 @@
 	@endforeach
 	</tbody>
 </table>
-
+@endsection
